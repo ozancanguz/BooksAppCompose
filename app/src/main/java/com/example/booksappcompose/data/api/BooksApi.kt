97d1e4fd003de8927dad3dc13075997829f7d571
ozancanguz <ozancanguz24@gmail.com>
2023-09-25 16:30:53 +0300

    BooksApi.kt interface created

---
+++ app/src/main/java/com/example/booksappcompose/data/api/BooksApi.kt
@@ -0,0 +1,16 @@
+package com.example.booksappcompose.data.api
+
+import com.example.booksappcompose.data.BookList
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+
+interface BooksApi {
+
+    @GET("newBook")
+    @Headers("authorization: apikey 2xAW88zA1iemGAxzWMNumI:47IyS3uICCasjqzWaFtxnn", "content-type: application/json")
+    suspend fun getBooks(): Response<BookList>
+
+
+
+}
