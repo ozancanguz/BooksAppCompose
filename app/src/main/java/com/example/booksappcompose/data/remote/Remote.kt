326ed412ec3843413b4348b9b1094dd6f46f32c7
ozancanguz <ozancanguz24@gmail.com>
2023-09-25 16:39:33 +0300

    remote data source created

---
+++ app/src/main/java/com/example/booksappcompose/data/remote/Remote.kt
@@ -0,0 +1,17 @@
+package com.example.booksappcompose.data.remote
+
+import com.example.booksappcompose.data.BookList
+import com.example.booksappcompose.data.api.BooksApi
+import retrofit2.Response
+import javax.inject.Inject
+
+class Remote@Inject constructor(private val booksApi: BooksApi ) {
+
+
+    suspend fun getBooks(): Response<BookList>{
+        return booksApi.getBooks()
+    }
+
+
+
+}
