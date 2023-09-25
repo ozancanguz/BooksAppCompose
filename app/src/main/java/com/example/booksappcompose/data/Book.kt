da264707e62711dee2ef06ab5785e2502f265687
ozancanguz <ozancanguz24@gmail.com>
2023-09-25 16:01:46 +0300

    data class created

---
+++ app/src/main/java/com/example/booksappcompose/data/Book.kt
@@ -0,0 +1,11 @@
+package com.example.booksappcompose.data
+
+data class Book(
+    val fiyat: String,
+    val image: String,
+    val indirim: String,
+    val title: String,
+    val url: String,
+    val yayın: String,
+    val yazar: String
+)
---
+++ app/src/main/java/com/example/booksappcompose/data/BookList.kt
@@ -0,0 +1,6 @@
+package com.example.booksappcompose.data
+
+data class BookList(
+    val result: List<Book>,
+    val success: Boolean
+)
